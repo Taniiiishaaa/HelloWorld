1c5f6ffd3e19678b43e6a432b3ba9868cab965e4
Tanisha Badgujar <155179674+Taniiiishaaa@users.noreply.github.com>
2025-09-16 14:59:27 -0700

    Add files via upload

---
+++ HelloWorldAction.java
@@ -0,0 +1,14 @@
+package actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class HelloWorldAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        Messages.showInfoMessage("Hello world!", "Info");
+    }
+
+}
